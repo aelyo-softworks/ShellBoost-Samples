63bf1daeeaa92930daf54987f605dd36eb3eda04
Simon Mourier <simon_mourier@hotmail.com>
2018-06-07 18:35:44 +0200

    Added support for Icons as Win32 resources.

--- ShellBoost.Samples/ShellBoost.Samples.LocalFolder/LocalShellFolderServer.cs
+++ ShellBoost.Samples/ShellBoost.Samples.LocalFolder/LocalShellFolderServer.cs
@@ -1,11 +1,17 @@
 ﻿using System;
 using System.IO;
+using System.Reflection;
 using ShellBoost.Core;
+using ShellBoost.Core.Utilities;
 
 namespace ShellBoost.Samples.LocalFolder
 {
     public class LocalShellFolderServer : ShellFolderServer
     {
+        public const int AttributeIconIndex = 100;
+        public const int ErrorOverlayIconIndex = 101;
+        public const int WarningOverlayIconIndex = 102;
+
         private RootFolder _root;
 
         public LocalShellFolderServer(DirectoryInfo info)
@@ -17,9 +23,18 @@ namespace ShellBoost.Samples.LocalFolder
                 throw new ArgumentException(null, nameof(info));
 
             Info = info;
+            IconsDllPath = Path.GetFullPath("icons.dll");
+            if (!File.Exists(IconsDllPath))
+            {
+                // use ShellBoost utility to extract .NET embedded resources and insert them into a Win32 resource-only .DLL file
+                IconUtilities.SaveAsDll(Assembly.GetExecutingAssembly(), typeof(Program).Namespace + ".Resources.Attribute.ico", IconsDllPath, AttributeIconIndex, 1033, false);
+                IconUtilities.SaveAsDll(Assembly.GetExecutingAssembly(), typeof(Program).Namespace + ".Resources.ErrorOverlay.ico", IconsDllPath, ErrorOverlayIconIndex, 1033, false);
+                IconUtilities.SaveAsDll(Assembly.GetExecutingAssembly(), typeof(Program).Namespace + ".Resources.WarningOverlay.ico", IconsDllPath, WarningOverlayIconIndex, 1033, false);
+            }
         }
 
         public DirectoryInfo Info { get; }
+        public string IconsDllPath { get; }
 
         protected override RootShellFolder GetRootFolder(ShellItemIdList idl)
         {
--- ShellBoost.Samples/ShellBoost.Samples.LocalFolder/XmlAttributeShellItem.cs
+++ ShellBoost.Samples/ShellBoost.Samples.LocalFolder/XmlAttributeShellItem.cs
@@ -1,5 +1,6 @@
 ﻿using System.Xml;
 using ShellBoost.Core;
+using ShellBoost.Core.WindowsPropertySystem;
 
 namespace ShellBoost.Samples.LocalFolder
 {
@@ -10,8 +11,35 @@ namespace ShellBoost.Samples.LocalFolder
         {
             Attribute = attribute;
             DisplayName = attribute.LocalName;
+            var iconsPath = ((RootFolder)parent.Root).Server.IconsDllPath;
+
+            // note the icon index syntax: the index must be negative when passed to the Windows Shell
+            Thumbnail = new ShellThumbnail(iconsPath, -LocalShellFolderServer.AttributeIconIndex);
         }
 
         public XmlAttribute Attribute { get; }
+
+        public override bool TryGetPropertyValue(PropertyKey key, out object value)
+        {
+            // OverlayIconLocation is not a Windows property, it's a ShellBoost special property
+            if (key == PropertyStore.OverlayIconLocation)
+            {
+                var iconsPath = ((RootFolder)Parent.Root).Server.IconsDllPath;
+                
+                // note the icon index syntax: the index must be negative when passed to the Windows Shell
+                switch (Attribute.Value)
+                {
+                    case "Error":
+                        value = iconsPath + ",-" + LocalShellFolderServer.ErrorOverlayIconIndex;
+                        return true;
+
+                    case "Warning":
+                        value = iconsPath + ",-" + LocalShellFolderServer.WarningOverlayIconIndex;
+                        return true;
+                }
+            }
+
+            return base.TryGetPropertyValue(key, out value);
+        }
     }
 }
