92870ff839b5ed784c43175cb54b72bae90f2ba2
Simon Mourier <simon_mourier@hotmail.com>
2019-02-05 17:45:25 +0100

    Updated to ShellBoost 1.2.2.0

--- ShellBoost.Samples/ShellBoost.Samples.RegistryFolder/RegistryBaseKeyFolder.cs
+++ ShellBoost.Samples/ShellBoost.Samples.RegistryFolder/RegistryBaseKeyFolder.cs
@@ -1,9 +1,14 @@
-﻿using Microsoft.Win32;
+﻿using System;
+using Microsoft.Win32;
+using ShellBoost.Core;
+using ShellBoost.Core.Utilities;
 
 namespace ShellBoost.Samples.RegistryFolder
 {
     public class RegistryBaseKeyFolder : RegistryKeyFolder
     {
+        private readonly static Guid _adminMessageGuid = Guid.NewGuid();
+
         public RegistryBaseKeyFolder(RootFolder parent, RegistryHive hive)
             : base(parent, GetName(hive))
         {
@@ -15,6 +20,35 @@ namespace ShellBoost.Samples.RegistryFolder
         public RegistryHive Hive { get; }
         public override string Path => null;
         public override RegistryBaseKeyFolder BaseParent => this;
+
+        protected override InformationBar GetInformationBar()
+        {
+            if (Hive != RegistryHive.LocalMachine || DiagnosticsInformation.GetTokenElevationType() == TokenElevationType.Full)
+                return null;
+
+            var bar = new InformationBar();
+            bar.Guid = _adminMessageGuid;
+            bar.Message = "You must be running with full priviledges to change anything here";
+            return bar;
+        }
+
+        protected override void CreateInformationBarMenu(InformationBar bar, ShellMenu appendMenu)
+        {
+            var item = new ShellMenuItem(appendMenu, "What is my UAC level?");
+            appendMenu.Items.Add(item);
+        }
+
+        protected override async void HandleInformationBarMenu(InformationBar bar, IntPtr hwndOwner, int id)
+        {
+            if (id == 1) // first item has id 1, etc.
+            {
+                await WindowsUtilities.DoModelessAsync(() =>
+                {
+                    System.Windows.Forms.MessageBox.Show(new Win32Window(hwndOwner), "UAC level is " + DiagnosticsInformation.GetTokenElevationType(), "Registry Folder");
+                });
+                return;
+            }
+        }
 
         private static string GetName(RegistryHive hive)
         {
