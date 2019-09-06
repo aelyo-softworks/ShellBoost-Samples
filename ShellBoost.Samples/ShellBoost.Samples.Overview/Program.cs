8e566c506d99bde41536f66c61fc60a661db992b
Simon Mourier <simon_mourier@hotmail.com>
2019-09-06 19:38:37 +0200

    Bumped to 1.2.7.1.

--- ShellBoost.Samples/ShellBoost.Samples.Overview/Program.cs
+++ ShellBoost.Samples/ShellBoost.Samples.Overview/Program.cs
@@ -7,7 +7,7 @@ namespace ShellBoost.Samples.Overview
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("ShellBoost Samples - Overview - Copyright (C) 2017-" + DateTime.Now.Year + " Aelyo Softworks. All rights reserved.");
+            Console.WriteLine("ShellBoost Samples - Overview - " + (IntPtr.Size == 4 ? "32" : "64") + "-bit - Copyright (C) 2017-" + DateTime.Now.Year + " Aelyo Softworks. All rights reserved.");
             Console.WriteLine();
 
             Console.WriteLine("Press a key:");
