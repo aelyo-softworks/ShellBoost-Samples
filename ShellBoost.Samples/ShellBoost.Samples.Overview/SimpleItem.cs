2b9361b539aa6592e036ad36c16d3eb69d210329
Aelyo Softworks <contact@aelyo.com>
2020-04-21 11:27:41 +0200

    Added tooltip sample.

--- ShellBoost.Samples/ShellBoost.Samples.Overview/SimpleItem.cs
+++ ShellBoost.Samples/ShellBoost.Samples.Overview/SimpleItem.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using ShellBoost.Core;
+using ShellBoost.Core.WindowsPropertySystem;
 
 namespace ShellBoost.Samples.Overview
 {
@@ -8,6 +9,18 @@ namespace ShellBoost.Samples.Overview
         public SimpleItem(RootFolder parent, string text)
             : base(parent, new StringKeyShellItemId(text))
         {
+        }
+
+        public override bool TryGetPropertyValue(PropertyKey key, out object value)
+        {
+            // dynamic infotip (aka: tooltip)
+            if (key == Core.WindowsPropertySystem.System.InfoTipText)
+            {
+                value = "This is item " + DisplayName;
+                return true;
+            }
+
+            return base.TryGetPropertyValue(key, out value);
         }
 
         public override ShellContent GetContent()
